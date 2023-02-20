c3ad3df47ce4fffc23e3cd66f0bf3c7ff0e59234
Noel Jose <noeljose@SSLAP0013.local>
2023-02-20 16:52:10 +0530

    Added form to create todo

--- js/app.js
+++ js/app.js
@@ -1,13 +1,30 @@
 let todos = [];
 
-const setTodos = (todo) =>{
-    todos.push(todo);
-    localStorage.setItem("todos",JSON.stringify(todos));
-}
+const setTodos = (todo) => {
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
 
-const getTodos = () =>{
-    todos = JSON.parse(localStorage.getItem("todos"));
-}
+const getTodos = () => {
+  todos = JSON.parse(localStorage.getItem("todos"));
+};
+
+
+addTodoButton = document.querySelector(".header__button");
+createTodoModal = document.querySelector(".modal");
+
+addTodoButton.addEventListener("click", () => {
+    console.log(createTodoModal.classList.toggle("showcreateTodoForm"));
+});
 
 
 
+const title = document.getElementById("title");
+
+
+
+
+
+const formSubmit = (todo, setTodo) => {
+
+};
