e630af18726cdd5f5ba488e73d29e629d7c674fd
Noel Jose <noeljose@SSLAP0013.local>
2023-02-20 17:32:46 +0530

    Added functionality to add created todos to the localstorage

--- js/app.js
+++ js/app.js
@@ -1,30 +1,40 @@
-let todos = [];
+const getTodos = () => {
+  let todosfromLS = JSON.parse(localStorage.getItem("todos"));
+  console.log(todosfromLS);  
+  return todosfromLS;
+};
+
+let todos = getTodos() || [];
 
 const setTodos = (todo) => {
-  todos.push(todo);
+  todos.unshift(todo);
   localStorage.setItem("todos", JSON.stringify(todos));
 };
-
-const getTodos = () => {
-  todos = JSON.parse(localStorage.getItem("todos"));
-};
-
 
 addTodoButton = document.querySelector(".header__button");
 createTodoModal = document.querySelector(".modal");
 
+// function to show the modal
 addTodoButton.addEventListener("click", () => {
-    console.log(createTodoModal.classList.toggle("showcreateTodoForm"));
+  console.log(createTodoModal.classList.toggle("showcreateTodoForm"));
 });
+//accessing the form to create todo
+const createTodoForm = document.querySelector(".todo__create");
 
+// function to perform when we submit the form
+createTodoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let todo = {};
+  todo.id = new Date().getTime();
+  todo.name = createTodoForm.elements[0].value;
+  todo.desc = createTodoForm.elements[1].value;
+  todo.completed = false;
+  console.log(todo);
+  setTodos(todo);
 
+  // added the todo to the
+});
 
 const title = document.getElementById("title");
 
-
-
-
-
-const formSubmit = (todo, setTodo) => {
-
-};
+const formSubmit = (todo, setTodo) => {};
